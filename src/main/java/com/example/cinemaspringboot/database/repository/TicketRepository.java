2e185ab621d7c0a1b8b09f45e2964b43453149d3
mmmurdered <viacheslawl@gmail.com>
2023-02-17 14:04:39 +0200

    TicketRepo fix

--- src/main/java/com/example/cinemaspringboot/database/repository/TicketRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.cinemaspringboot.database.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TicketRepository extends JpaRepository<TicketRepository, Integer> {
-}
