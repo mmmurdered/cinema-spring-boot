036f8a40390744528c7ce6b79f40e2aded5ce185
Vyacheslav <viacheslawl@gmail.com>
2023-03-06 22:31:51 +0200

    Auth for buying tickets

--- src/main/java/com/example/cinemaspringboot/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/cinemaspringboot/configuration/SecurityConfiguration.java
@@ -5,9 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -30,6 +28,10 @@ public class SecurityConfiguration {
                     .requestMatchers("/film/update/**", "/film/delete/**, " +
                             "/session/update/**, /session/delete/**")
                     .hasAuthority("ADMIN")
+                .and()
+                    .authorizeHttpRequests()
+                    .requestMatchers("/ticket/buy/**")
+                    .hasAuthority("USER")
                 .and()
                     .formLogin()
                     .loginPage("/login")
