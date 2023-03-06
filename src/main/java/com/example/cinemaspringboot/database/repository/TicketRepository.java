98e1d14cd01d66f6f602ee9a28db8e11ae69494c
Vyacheslav <viacheslawl@gmail.com>
2023-03-06 22:28:08 +0200

    Raw version of buying tickets

--- src/main/java/com/example/cinemaspringboot/database/repository/TicketRepository.java
+++ src/main/java/com/example/cinemaspringboot/database/repository/TicketRepository.java
@@ -1,13 +1,11 @@
 package com.example.cinemaspringboot.database.repository;
 
-import com.example.cinemaspringboot.database.entity.Seat;
 import com.example.cinemaspringboot.database.entity.Session;
 import com.example.cinemaspringboot.database.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
--- src/main/java/com/example/cinemaspringboot/wrapper/SeatListWrapper.java
+++ src/main/java/com/example/cinemaspringboot/wrapper/SeatListWrapper.java
@@ -3,8 +3,6 @@ package com.example.cinemaspringboot.wrapper;
 import com.example.cinemaspringboot.database.entity.Seat;
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 @Data
